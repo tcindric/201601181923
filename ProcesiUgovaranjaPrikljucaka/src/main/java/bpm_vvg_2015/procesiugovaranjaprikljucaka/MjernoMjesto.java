6a78f89e8b81c19de949e61e03e24618b4fc0f64
admin <>
2015-11-24 21:39:32 +0100

    {/ProcesiUgovaranjaPrikljucaka/src/main/java/bpm_vvg_2015/procesiugovaranjaprikljucaka/MjernoMjesto.java}

--- ProcesiUgovaranjaPrikljucaka/src/main/java/bpm_vvg_2015/procesiugovaranjaprikljucaka/MjernoMjesto.java
+++ ProcesiUgovaranjaPrikljucaka/src/main/java/bpm_vvg_2015/procesiugovaranjaprikljucaka/MjernoMjesto.java
@@ -9,12 +9,15 @@ public class MjernoMjesto implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Label(value = "Broj_mjernog_mjesta")
+   @org.kie.api.definition.type.Label("Broj_mjernog_mjesta")
    private java.lang.String brojMjernogMjesta;
-   @org.kie.api.definition.type.Label(value = "Datum_aktiviranja")
+   @org.kie.api.definition.type.Label("Datum_aktiviranja")
    private java.util.Date datumAktiviranja;
-   @org.kie.api.definition.type.Label(value = "Broj_vodomjera")
+   @org.kie.api.definition.type.Label("Broj_vodomjera")
    private java.lang.String brojVodomjera;
+
+   @org.kie.api.definition.type.Label(value = "Aktiviran_korisnik")
+   private java.lang.Boolean aktiviranKorisnik;
 
    public MjernoMjesto()
    {
@@ -50,12 +53,24 @@ public class MjernoMjesto implements java.io.Serializable
       this.brojVodomjera = brojVodomjera;
    }
 
+   public java.lang.Boolean getAktiviranKorisnik()
+   {
+      return this.aktiviranKorisnik;
+   }
+
+   public void setAktiviranKorisnik(java.lang.Boolean aktiviranKorisnik)
+   {
+      this.aktiviranKorisnik = aktiviranKorisnik;
+   }
+
    public MjernoMjesto(java.lang.String brojMjernogMjesta,
-         java.util.Date datumAktiviranja, java.lang.String brojVodomjera)
+         java.util.Date datumAktiviranja, java.lang.String brojVodomjera,
+         java.lang.Boolean aktiviranKorisnik)
    {
       this.brojMjernogMjesta = brojMjernogMjesta;
       this.datumAktiviranja = datumAktiviranja;
       this.brojVodomjera = brojVodomjera;
+      this.aktiviranKorisnik = aktiviranKorisnik;
    }
 
 }
